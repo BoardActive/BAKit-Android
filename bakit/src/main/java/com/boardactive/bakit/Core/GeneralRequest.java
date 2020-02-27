6961ee77e1bdeed2b5fa2de3088089680242946c
AxiomAim <tom@axiomaim.com>
2020-02-27 15:31:09 -0500

    More Cleanup

--- bakit/src/main/java/com/boardactive/bakit/Core/GeneralRequest.java
+++
@@ -1,9 +0,0 @@
-package com.boardactive.bakit.Core;
-
-/**
- * Generic interface for api request
- * Created by lipy on 9/18/2017.
- */
-
-public interface GeneralRequest {
-}
--- bakit/src/main/java/com/boardactive/bakit/Core/GeneralResponse.java
+++
@@ -1,9 +0,0 @@
-package com.boardactive.bakit.Core;
-
-/**
- * Created by lipy on 9/11/2017.
- */
-
-public class GeneralResponse {
-
-}
