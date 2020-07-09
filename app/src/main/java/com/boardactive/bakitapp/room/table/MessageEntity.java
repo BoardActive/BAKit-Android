cec19a33923b0a0c32db15c976930d8735077e43
axiomaim <tom@axiomaim.com>
2020-07-09 17:28:06 -0400

    sync

--- app/src/main/java/com/boardactive/bakitapp/room/table/MessageEntity.java
+++ app/src/main/java/com/boardactive/bakitapp/room/table/MessageEntity.java
@@ -5,6 +5,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.boardactive.bakit.models.MessageModel;
+
 import java.io.Serializable;
 
 /*
